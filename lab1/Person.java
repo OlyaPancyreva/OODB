8a48c4f3118d9f5f06ea120a43bc5a342e6efca3
OlyaPancyreva <55744134+OlyaPancyreva@users.noreply.github.com>
2019-09-30 11:41:20 +0300

    Create Person.java

---
+++ lab1/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+
+    private String name;
+    private String number;
+    private char gender;
+    private double salary;
+
+
+    Person(String name, String number, char gender, double salary) {
+        this.name = name;
+        this.number = number;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+}
